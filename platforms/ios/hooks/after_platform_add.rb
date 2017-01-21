6eef194002d761812b26cc2d0c4bfeff71869dfb
Kunio Sawatani <devel@fathens.org>
2017-01-21 13:57:56 +0900

    plugin.xml からも podspec を削除する
    Briding-Header が追加されてしまうのを防ぎます。

--- platforms/ios/hooks/after_platform_add.rb
+++ platforms/ios/hooks/after_platform_add.rb
@@ -12,7 +12,21 @@ end
 
 def remove_answers(my_id, other_id)
     return unless plugin_xml(other_id).exist?
-    return unless plugin_xml(my_id).exist?
+
+    pluginxml = plugin_xml my_id
+    return unless pluginxml.exist?
+
+    puts "Modify #{pluginxml}"
+    xml = File.open(pluginxml) {|src| REXML::Document.new src }
+    xml.get_elements('//platform[@name="ios"]').each { |platform|
+        platform.get_elements('framework[@src="Answers"]').each { |e|
+            puts "Deleting: #{e}"
+            platform.delete_element e
+        }
+    }
+    File.open(pluginxml, 'w') { |dst|
+        xml.write dst
+    }
 
     podfile = $PLATFORM_DIR/'Podfile'
     puts "Modify #{podfile}"
